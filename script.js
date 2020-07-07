b9887c12803379e4a0fc99f4df78da424eec2a32
Queen of Corgis <tana.owens@gmail.com>
2020-07-07 16:06:23 -0600

    started to build js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=${APIKey}&units=imperial` -- link to open weather API
+// `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=${APIKey}&units=imperial` -- link to open weather API/
+
+var apiKey = "436bc66d9934aa2b0814e3e5a007aaf5";
+"api.openweathermap.org/data/2.5/weather?q=" + "&appid=" + apiKey;
+
+$("#date").text()
+
+
+
+function get1day() {
+    $.ajax ({
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=denver&appid=436bc66d9934aa2b0814e3e5a007aaf5',
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        
+    }
+    ) 
+}
+
+$("#search-button").on("click", get1day)
