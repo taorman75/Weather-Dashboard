571232d312dec455b441deac0154e1909beb1b11
Queen of Corgis <tana.owens@gmail.com>
2020-07-08 16:12:01 -0600

    added more functionality

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ $(".input-group-text").on("click", getOneDay)
   }).then(function(response){
     console.log(response);
     $("#current-city").text(city);
+    var icon = $("img").text(response.weather.icon);
+    $("#current-city").append(response.weather.icon);
     var temp = $("<p>").text(response.main.temp);
     $(".jumbotron").append("Current temperature: ", temp, "°F");
     console.log(temp);
@@ -41,14 +43,56 @@ $(".input-group-text").on("click", getOneDay)
     
     var uvQuery = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${long}`;
     console.log(uvQuery);
-    
-   // var uvIndex = $("<p>").text()  see https://openweathermap.org/api/uvi
+    $.ajax({
+      url: uvQuery,
+      method: "GET"
+    }).then(function(uvResponse) {
+      console.log(uvResponse.value);
+     $(".jumbotron").append("UV Index: ", uvResponse.value); 
+    })
+     var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+    console.log(fiveDayForecast);  
+
+    $.ajax({
+      url: fiveDayForecast,
+      method: "GET"
+    }).then(function(forecast){
+      console.log(forecast);
+      var fTemp1 = $("<p>").text(forecast.list[5].main.temp);
+      var fHumid1 = $("<p>").text(forecast.list[5].main.humidity);
+      $("#1").append(fTemp1);
+      $("#1").append(fHumid1);
+
+      var fTemp2 = $("<p>").text(forecast.list[13].main.temp);
+      console.log(fTemp2);
+      var fHumid2 = $("<p>").text(forecast.list[13].main.humidity);
+      $("#2").append(fTemp2);
+      $("#2").append(fHumid2);
+
+      var fTemp3 = $("<p>").text(forecast.list[21].main.temp);
+      var fHumid3 = $("<p>").text(forecast.list[21].main.humidity);
+      $("#3").append(fTemp3);
+      $("#3").append(fHumid3);
+
+      var fTemp4 = $("<p>").text(forecast.list[29].main.temp);
+      var fHumid4 = $("<p>").text(forecast.list[29].main.humidity);
+      $("#4").append(fTemp4);
+      $("#4").append(fHumid4);
+
+      var fTemp5 = $("<p>").text(forecast.list[37].main.temp);
+      var fHumid5 = $("<p>").text(forecast.list[37].main.humidity);
+      $("#5").append(fTemp5);
+      $("#5").append(fHumid5);
+    }
+    )
+   
+    // $("#date").text(moment().format("MM / DD / YY"));
     
   
   });
    
   }
 
-  function getFiveDay (params) {
-      
-  }
+  // function getFiveDay (params) {
+   
+  // }
