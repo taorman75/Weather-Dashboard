f4b567271b9d1cd7b8c0ddeed0b3fc7339e56cc3
Queen of Corgis <tana.owens@gmail.com>
2020-07-09 15:53:02 -0600

    fixed forecast dates with momentJS

--- script.js
+++ script.js
@@ -77,7 +77,8 @@ $(".input-group-text").on("click", getOneDay)
     }).then(function(forecast){
 
       $("#1").empty();
-      var date1 = $("<p>").text(moment().format("MM/DD/YY")); // <<< can't manipulate date
+      console.log(forecast);
+      var date1 = $("<p>").text(moment(forecast.list[5].dt_txt).format("MM/DD/YY"));
       var fTemp1 = $("<p>").text("High: " + forecast.list[5].main.temp + "°F");
       var fHumid1 = $("<p>").text("Humidity: " + forecast.list[5].main.humidity + "%");
       var icon1 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[5].weather[0].icon + ".png");
@@ -87,33 +88,41 @@ $(".input-group-text").on("click", getOneDay)
       $("#1").append(fHumid1);
 
       $("#2").empty();
+      var date2 = $("<p>").text(moment(forecast.list[13].dt_txt).format("MM/DD/YY"));
       var fTemp2 = $("<p>").text("High: " + forecast.list[13].main.temp + "°F");
       var fHumid2 = $("<p>").text("Humidity: " + forecast.list[13].main.humidity + "%");
       var icon2 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[13].weather[0].icon + ".png");
+      $("#2").append(date2);
       $("#2").append(fTemp2);
       $("#2").append(icon2);
       $("#2").append(fHumid2);
 
       $("#3").empty();
+      var date3 = $("<p>").text(moment(forecast.list[21].dt_txt).format("MM/DD/YY"));
       var fTemp3 = $("<p>").text("High: " + forecast.list[21].main.temp + "°F");
       var fHumid3 = $("<p>").text("Humidity: " + forecast.list[21].main.humidity + "%");
       var icon3 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[21].weather[0].icon + ".png");
+      $("#3").append(date3);
       $("#3").append(fTemp3);
       $("#3").append(icon3);
       $("#3").append(fHumid3);
 
       $("#4").empty();
+      var date4 = $("<p>").text(moment(forecast.list[29].dt_txt).format("MM/DD/YY"));
       var fTemp4 = $("<p>").text("High: " + forecast.list[29].main.temp + "°F");
       var fHumid4 = $("<p>").text("Humidity: " + forecast.list[29].main.humidity + "%");
       var icon4 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[29].weather[0].icon + ".png");
+      $("#4").append(date4);
       $("#4").append(fTemp4);
       $("#4").append(icon4);
       $("#4").append(fHumid4);
 
       $("#5").empty();
+      var date5 = $("<p>").text(moment(forecast.list[37].dt_txt).format("MM/DD/YY"));
       var fTemp5 = $("<p>").text("High: " + forecast.list[37].main.temp + "°F");
       var fHumid5 = $("<p>").text("Humidity: " + forecast.list[37].main.humidity + "%");
       var icon5 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[37].weather[0].icon + ".png");
+      $("#5").append(date5);
       $("#5").append(fTemp5);
       $("#5").append(icon5);
       $("#5").append(fHumid5);
