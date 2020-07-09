bbbe9b89622d919e6d890a7a3e3c789e3eed6ea3
Queen of Corgis <tana.owens@gmail.com>
2020-07-08 20:36:17 -0600

    fixed icons and click event for previous cities

--- script.js
+++ script.js
@@ -5,23 +5,35 @@ $(".input-group-text").on("click", getOneDay)
 
     // event.preventDefault();
 
-  // $(".btn-block").on("click", function(){     <<<< Unclear
-  //   var city = $("btn.block").val();
-  //   getOneDay();
-  // })  
+   
     
   
 
   function getOneDay (params) {
    // Here we grab the text from the input box
-    // $(".jumbotron").text("");  <<< to hide previous city information
-    // $(".card").text("");
+    $(".jumbotron").html("");  
+    var newH1 = $("<h1>");
+    newH1.attr("id", "current-city")
+    $(".jumbotron").append(newH1);
+    // $(".card").html("");
     var city = $("#city-input").val();
     var searched = $("<button>").text(city);
-    searched.addClass(".btn-block");
+    searched.addClass("btn-block");
+    searched.attr('id', 'historyS')
+    searched.val(city)
     $(".col-md-3").append(searched);
+   /* $('#historyS').click(function () {
+      alert('1')
+    })*/
     
-
+      $(".btn-block").click(function(){    // <<<< Unclear
+        //alert('1')
+        
+        var city = $(this).val()
+        console.log(city);
+               
+       // getOneDay();
+      }) 
     // Here we construct our URL
     var apiKey = "1e371b6b2c7be9361517f7c932a1a6c8";
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
@@ -33,10 +45,10 @@ $(".input-group-text").on("click", getOneDay)
   }).then(function(response){
     console.log(response);
     $("#current-city").text(city);
-    var date = $("<h3>").text(moment().format("MM / DD / YY"));
+    var date = $("<h3>").text(moment().format("MM/DD/YY"));
     $(".jumbotron").append(date);
-    var icon = $("<img>").text(response.weather.icon);
-    $("#current-city").append(response.weather.icon);
+    var icon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");  // https://openweathermap.org/img/w/04n.png
+    $("#current-city").append(icon);
     var temp = $("<p>").text("Current temperature: " + response.main.temp + "°F");
     $(".jumbotron").append(temp);
     var humid = $("<p>").text("Humidity: " + response.main.humidity + "%");
@@ -54,7 +66,8 @@ $(".input-group-text").on("click", getOneDay)
       method: "GET"
     }).then(function(uvResponse) {
       console.log(uvResponse.value);
-     $(".jumbotron").append("UV Index: ", uvResponse.value); 
+      var uvIndex = ("<p>").text("UV Index: " + uvResponse.value)
+     $(".jumbotron").append(uvIndex); 
     })
      var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
     console.log(fiveDayForecast);  
@@ -66,28 +79,38 @@ $(".input-group-text").on("click", getOneDay)
       console.log(forecast);
       var fTemp1 = $("<p>").text("High: " + forecast.list[5].main.temp + "°F");
       var fHumid1 = $("<p>").text("Humidity: " + forecast.list[5].main.humidity + "%");
+      var icon1 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[5].weather[0].icon + ".png");
       $("#1").append(fTemp1);
+      $("#1").append(icon1);
       $("#1").append(fHumid1);
 
       var fTemp2 = $("<p>").text("High: " + forecast.list[13].main.temp + "°F");
       console.log(fTemp2);
       var fHumid2 = $("<p>").text("Humidity: " + forecast.list[13].main.humidity + "%");
+      var icon2 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[13].weather[0].icon + ".png");
       $("#2").append(fTemp2);
+      $("#2").append(icon2);
       $("#2").append(fHumid2);
 
       var fTemp3 = $("<p>").text("High: " + forecast.list[21].main.temp + "°F");
       var fHumid3 = $("<p>").text("Humidity: " + forecast.list[21].main.humidity + "%");
+      var icon3 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[21].weather[0].icon + ".png");
       $("#3").append(fTemp3);
+      $("#3").append(icon3);
       $("#3").append(fHumid3);
 
       var fTemp4 = $("<p>").text("High: " + forecast.list[29].main.temp + "°F");
       var fHumid4 = $("<p>").text("Humidity: " + forecast.list[29].main.humidity + "%");
+      var icon4 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[29].weather[0].icon + ".png");
       $("#4").append(fTemp4);
+      $("#4").append(icon4);
       $("#4").append(fHumid4);
 
       var fTemp5 = $("<p>").text("High: " + forecast.list[37].main.temp + "°F");
       var fHumid5 = $("<p>").text("Humidity: " + forecast.list[37].main.humidity + "%");
+      var icon5 = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecast.list[37].weather[0].icon + ".png");
       $("#5").append(fTemp5);
+      $("#5").append(icon5);
       $("#5").append(fHumid5);
     }
     )
